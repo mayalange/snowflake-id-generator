553a0555708135d0fde9f846dcb1dfab6aa392c1
marina <18-9292@mail.ru>
2025-12-03 20:24:18 +0300

    hw 5 added logs for controller

--- src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
@@ -1,10 +1,22 @@
 package io.maksymdobrynin.snowflakegenerator
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/v1")
 class SnowflakeController(private val generator: Generator) {
+	private val logger = LoggerFactory.getLogger(SnowflakeController::class.java)
+
 	@GetMapping("/next-id")
-	suspend fun generate(): Long = generator.nextId()
+	suspend fun generate(): Long {
+		val podName = System.getenv("HOSTNAME") ?: "unknown"
+		val id = generator.nextId()
+
+		logger.info("Pod '$podName' generated ID: $id")
+
+		println("[$podName] $id")
+
+		return id
+	}
 }
