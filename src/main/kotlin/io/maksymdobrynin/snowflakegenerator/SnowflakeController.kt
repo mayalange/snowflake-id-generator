a817fc0dafac004f6d88fd2f5b997ef8e27655de
marina <18-9292@mail.ru>
2025-12-03 21:10:04 +0300

    hw 5 fix logging

--- src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
+++ src/main/kotlin/io/maksymdobrynin/snowflakegenerator/SnowflakeController.kt
@@ -1,22 +1,10 @@
 package io.maksymdobrynin.snowflakegenerator
 
-import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController("/v1")
 class SnowflakeController(private val generator: Generator) {
-	private val logger = LoggerFactory.getLogger(SnowflakeController::class.java)
-
 	@GetMapping("/next-id")
-	suspend fun generate(): Long {
-		val podName = System.getenv("HOSTNAME") ?: "unknown"
-		val id = generator.nextId()
-
-		logger.info("Pod '$podName' generated ID: $id")
-
-		println("[$podName] $id")
-
-		return id
-	}
+	suspend fun generate(): Long = generator.nextId()
 }
