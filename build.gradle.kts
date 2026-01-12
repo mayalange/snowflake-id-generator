47f6fd03f0be98fd6bac1f19f6a26a1cd291e9bd
marina <18-9292@mail.ru>
2026-01-12 20:50:24 +0300

    hw 10

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,8 @@ plugins {
 	id("org.jlleitschuh.gradle.ktlint") version libs.versions.gradle.ktlint
 	id("io.gitlab.arturbosch.detekt") version libs.versions.gradle.detekt
 	kotlin("jvm") version libs.versions.kotlin
+	kotlin("plugin.spring") version libs.versions.kotlin
+	kotlin("plugin.jpa") version libs.versions.kotlin
 }
 
 apply(plugin = "io.spring.dependency-management")
@@ -22,6 +24,7 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation(libs.kotlin.stdlib)
 	implementation(libs.kotlinx.coroutines)
 	implementation(libs.springframework.boot.starter.web)
@@ -29,6 +32,8 @@ dependencies {
 	testImplementation(libs.junit.jupiter)
 	testImplementation(libs.assertj.core)
 	testImplementation(libs.kotlinx.coroutines.test)
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.postgresql:postgresql:42.7.3")
 }
 
 tasks.withType<KotlinCompile> {
